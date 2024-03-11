4d60702eb77ff0b354834e917b3979353f9ab0df
kapil-win <kapilmohan13@gmail.com>
2024-03-11 21:46:53 +0530

    yahoo finance

--- ModelPredictor.py
+++ ModelPredictor.py
@@ -1,20 +1,10 @@
 
 import pickle
 
-import numpy as np
-
-filename = "models//" + "Lasso_model.sav"
-# filename = "models//" + "SVR_hyp2.sav"
-
+filename = "models//" + "SVR_hyp.sav"
 pickled_model = pickle.load(open(filename, 'rb'))
-# check = [[3.98,  6.67,  0.31,  0.96,  7.79,  0.95]]
-# check = [[11.06,12.28,0.00054000004,0.73857003,0.36269,13.698857,0.97,0.89,1,5]]  #15.06
-
-npd = np.array([11.06,12.28,0.00054000004,0.73857003,0.36269,13.698857,0.97,0.89,1,5])
-y_pred = pickled_model.predict(npd)
-#23.354195902231304
-
-# y_pred = pickled_model.predict(check)
-#29.88432455
+check = [[3.98,  6.67,  0.31,  0.96,  7.79,  0.95]]
+check = [[75.28, 1.05, 18.34, 9.66, 24.55, 13.06]]
+y_pred = pickled_model.predict(check)
 
 print(y_pred)
