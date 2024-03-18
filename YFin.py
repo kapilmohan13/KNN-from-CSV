3eca4e39df4409e4cbd219b4bf01dd445b6164b6
kapil-win <kapilmohan13@gmail.com>
2024-03-18 22:07:02 +0530

    yahoo finance

--- YFin.py
+++ YFin.py
@@ -9,35 +9,49 @@ import datetime
 
 
 # READ  TICKERS TO SCRAP FROM YAHOO FINANCE
-sp_500 = pd.read_csv("data//yahoo.csv")
+sp_500 = pd.read_csv("data//BSE_tickers.csv")
 newList = []  # this will contain the out fields with ESG rating post iteration on tickers
 
-for symbol in sp_500["Symbol"]:
-    # Here We are getting Facebook financial information
-    # We need to pass FB as argument for that
-    GetFacebookInformation = yahooFinance.Ticker(symbol)
-    obj = GetFacebookInformation.info
-    # newTickerdf = pd.DataFrame(obj)
-    # print(newTickerdf)
+for symbol in sp_500["SYMBOL"]:
+    try:
+        # Here We are getting Facebook financial information
+        # We need to pass FB as argument for that
+        print("Processing symbol: "+symbol)
+        GetFacebookInformation = yahooFinance.Ticker(symbol)
+        obj = GetFacebookInformation.info
+        # newTickerdf = pd.DataFrame(obj)
+        # print(newTickerdf)
 
-    df = yesg.get_historic_esg(symbol)
-    if df is None:
-        continue
-    last_row = df.iloc[-1:]
-    esg_dict = {"ESG_score": last_row.iloc[0, 0]}
-    print(esg_dict)
-    obj.update(esg_dict)
+        df = yesg.get_historic_esg(symbol)
+        if df is None:
+            continue
+        last_row = df.iloc[-1:]
+        esg_dict = {"ESG_score": last_row.iloc[0, 0]}
+        print(esg_dict)
+        obj.update(esg_dict)
 
-    newObjDict = {"trailingEps": obj.get("trailingEps"), "forwardEps": obj.get("forwardEps"),
-                  "ESG_score": obj.get("ESG_score")}
+        newObjDict = {"trailingEps": obj.get("trailingEps"), "forwardEps": obj.get("forwardEps"),
+                      "heldPercentInsiders": obj.get("heldPercentInsiders"),
+                      "heldPercentInstitutions": obj.get("heldPercentInstitutions"),
+                      "profitMargins": obj.get("profitMargins"),
+                      "priceToSalesTrailing12Months": obj.get("priceToSalesTrailing12Months"),
+                      "payOutRatio": obj.get("payOutRatio"),
+                      "fiveYearAvgDividendYield": obj.get("fiveYearAvgDividendYield"), "beta": obj.get("beta"),
+                      "overallRisk": obj.get("overallRisk"), "boardRisk": obj.get("boardRisk"),
+                      "ESG_score": obj.get("ESG_score")}
+        #heldPercentInsiders, heldPercentInstitutions, profitMargins, priceToSalesTrailing12Months, payoutRatio,
+        #fiveYearAvgDividendYield, beta, overallRisk, boardRisk
 
+        print(newObjDict)
 
-    print(newObjDict)
-
-    newList.append(newObjDict)
+        newList.append(newObjDict)
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        print(f"Exception type: {type(e)}")
 
 # Write to file
-header_info = ["trailingEps", "forwardEps", "ESG_score"]
+header_info = ["trailingEps", "forwardEps", "heldPercentInsiders", "heldPercentInstitutions", "profitMargins",
+               "priceToSalesTrailing12Months", "payOutRatio", "fiveYearAvgDividendYield", "beta", "overallRisk", "boardRisk", "ESG_score"]
 with open('data//ESG_yfin.csv', 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=header_info)
     writer.writeheader()
